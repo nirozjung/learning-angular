60e5d403e28bd63f0e0c5dd062e2b9be41107bee
nirozjung <zorinzung@gmail.com>
2020-07-19 08:31:48 +0200

    basic services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,9 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { DishService } from './services/dish.service';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,7 +35,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatButtonModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ DishService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes'; 
+import { DISHES } from '../shared/dishes';
+import { DishService } from '../services/dish.service';
+ 
 
 @Component({
   selector: 'app-menu',
@@ -10,12 +12,13 @@ import { DISHES } from '../shared/dishes';
 
 export class MenuComponent implements OnInit {
 
-  dishes: Dish[] = DISHES;
+  dishes: Dish[];
   selectedDish:  Dish;
 
-  constructor() { }
+  constructor(private dishService: DishService) { }
 
   ngOnInit(): void {
+    this.dishes = this.dishService.getDishes();
 
   }
 
